a761fc667859f50bb6b3d518fc9dcd288fc51293
woo <wjswoo3@gmail.com>
2025-04-17 12:13:35 +0900

    데이터베이스 연결과 DB모델

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -15,7 +15,7 @@ from pydantic import BaseModel, Field  # 데이터를 깔끔하게 다루기 위
 
 class TaskBase(BaseModel):
     title: str | None = Field(
-        None,  # 제목이 없을 수도 있으니 기본값은 None 으로 설정
+        default=None,  # 제목이 없을 수도 있으니 기본값은 None 으로 설정
         examples=["세탁소에 맡긴 것을 찾으러 가기"],  # 예시 제목을 보여준다.
     )
     # title: 할 일의 제목 (str 또는 None)
@@ -51,30 +51,10 @@ class Task(TaskBase):  # '할 일'을 표현할 수 있는 Task라는 틀을 만
     id: int  # 할 일 번호(정수)
 
     done: bool = Field(
-        False,  # 처음에는 완료되지 않음(False)'으로 시작한다.
-        description="완료 플래그",  # True면 완료, False면 미완료를 나타냄
+        default=False,  # 처음에는 완료되지 않음(False)'으로 시작한다.
+        description="이 할 일이 완료되었는지 여부부",  # True면 완료, False면 미완료를 나타냄
     )
     # done: 이 할 일이 끝났는지를 표시하는 값 (True 또는 False만 가능함)
 
     class Config:
         orm_mode = True  # ORM 모델(SQLAlchemy 등)을 사용할 수 있도록 설정
-
-
-class Task(BaseModel):  # '할 일'을 표현할 수 있는 Task라는 틀을 만든다.
-
-    id: int  # 할 일 번호 (정수)
-
-    title: str | None = Field(
-        None,  # 아무 값이 없을 수도 있으니 기본값은 None으로 둔다.
-        examples=["세탁소에 맡긴 것을 찾으러 가기"],  # 예시 제목을 보여준다.
-    )
-
-    # title: 할 일의 제목
-    # str 또는 None -> 글자이거나 비어 있을 수도 있다.ss
-
-    done: bool = Field(
-        False,  # 처음에는 '완료되지 않음(False)'으로 시작한다.
-        description="True면 완료, False면 미완료",  # 무슨 뜻인지 설명해준다.
-    )
-
-    # done: 이 할 일이 끝났는지를 표시하는 겂 (True 또는 False만 가능함)
